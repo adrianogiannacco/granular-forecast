0fff4e628fbf642ac087108c340d8847b4320e4e
Augustin Riedinger <augustin.riedinger@gmail.com>
2022-04-04 11:51:38 +0200

    some more boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      color="secondary"
+      clipped-left
+      fixed
+      app
+      dark
+      flat
+    >
+      <v-toolbar-title>
+        <img alt="Granular logo" src="./assets/granular-logo.png">
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+       <WeatherForecast />
+    </v-main>
+
+    <v-footer app>
+      <div class="text-center" style="width: 100%;">
+        Granular Energy &copy; {{ new Date().getFullYear() }}
+      </div>
+    </v-footer>
+
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherForecast from './components/WeatherForecast.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherForecast
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  .v-app-bar {
+    img {
+      max-height: 2.5rem;
+      display: block;
+    }
+  }
 </style>
---
+++ src/components/WeatherForecast.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+
+    <p class="text-center my-12 text-h4">
+      Is it 🌞 today?
+    </p>
+
+    <div class="mx-12">
+      <highcharts :options="chartOptions"></highcharts>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import {Chart} from 'highcharts-vue'
+
+export default {
+  components: {
+    highcharts: Chart
+  },
+  data() {
+    return {
+      chartOptions: {
+        title: 'Example',
+        series: [{
+          data: [4, 2, 1.25, 1, 1.25, 2, 4] // sample data
+        }]
+      }
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
